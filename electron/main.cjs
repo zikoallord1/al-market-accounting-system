06fb6ec033f38612de45dabbffbe4ea23a1fc267
zikoallord1 <zikoallord@gmail.com>
2026-09-18 13:32:41 +0300

    build: add desktop mobile and offline app wrappers

---
+++ electron/main.cjs
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    webPreferences: { contextIsolation: true, nodeIntegration: false }
+  });
+  win.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+}
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
+});
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
