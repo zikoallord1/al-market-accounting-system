f42f2b2609d8b33984fa509a3971f8d8c105f269
zikoallord1 <zikoallord@gmail.com>
2026-09-18 13:24:25 +0300

    test: add sync server integration checks

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  build: { outDir: 'dist', sourcemap: false },
-  server: { host: '0.0.0.0', port: 5173 }
-});
